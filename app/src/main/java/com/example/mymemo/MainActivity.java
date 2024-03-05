06a704f19b812e1c25ce8b11c5a9eabdf35c82de
m3na0 <m3na02@gmail.com>
2024-03-05 19:04:33 +0200

    Added login and register functionality

--- app/src/main/java/com/example/mymemo/MainActivity.java
+++ app/src/main/java/com/example/mymemo/MainActivity.java
@@ -2,15 +2,47 @@ package com.example.mymemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginButton;
+    private Button registerButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        loginButton = findViewById(R.id.login);
+        registerButton = findViewById(R.id.register);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+    }
+    public void login(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public void register(){
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
 
     }
 }
