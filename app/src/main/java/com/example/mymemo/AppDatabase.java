0a85b99b5f4b18fc7e145d886343b65e75499044
Ciaran Cruz <ciarancaesarcruz@gmail.com>
2024-03-17 12:44:48 +0900

    instantiated database

--- app/src/main/java/com/example/mymemo/AppDatabase.java
+++ app/src/main/java/com/example/mymemo/AppDatabase.java
@@ -1,6 +1,9 @@
 package com.example.mymemo;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {User.class, DiaryEntry.class, DiaryTag.class, Tag.class}, version = 1)
@@ -9,4 +12,19 @@ public abstract class AppDatabase extends RoomDatabase{
     public abstract DiaryEntryDao diaryEntryDao();
     public abstract DiaryTagDao diaryTagDao();
     public abstract TagDao tagDao();
+
+    private static volatile AppDatabase INSTANCE;
+    static AppDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room
+                            .databaseBuilder(context.getApplicationContext()
+                            , AppDatabase.class, "APP_DB")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
